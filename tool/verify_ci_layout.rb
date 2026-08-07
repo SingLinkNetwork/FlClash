5aa489cf2586ec5a8b710b7ba0e78cec2ccd0f9d
SingLinkNetwork <developer@singlink.net>
2026-08-07 22:16:02 +0800

    fix: keep Android proxy service alive in background

--- tool/verify_ci_layout.rb
+++ tool/verify_ci_layout.rb
@@ -35,6 +35,7 @@ expected_scripts = %w[
   tool/verify_android_background_location_permission.rb
   tool/verify_android_tile_background_toggle.rb
   tool/verify_android_proxy_stop.rb
+  tool/verify_android_service_lifecycle.rb
   tool/verify_linux_x11_thread_init.rb
   tool/verify_windows_maximize_work_area.rb
   tool/verify_windows_proxy_shutdown.rb
