6b4abf1b7f8946381dc1fedec984d477a0e7a30a
SingLinkNetwork <developer@singlink.net>
2026-08-08 06:18:06 +0800

    test: verify shell environment menu in CI

--- tool/verify_ci_layout.rb
+++ tool/verify_ci_layout.rb
@@ -25,6 +25,7 @@ required_jobs = %w[
   macos-ip-forwarding
   custom-test-urls
   windows-core-callback
+  proxy-environment
   static-source
   linux-packaging
   build
@@ -50,6 +51,7 @@ expected_scripts = %w[
   tool/verify_window_visibility_queue.rb
   tool/verify_linux_tray_clipboard.rb
   tool/verify_windows_paste_fix.rb
+  tool/verify_proxy_environment_menu.rb
   tool/verify_windows_search_close.rb
   tool/verify_linux_renderer_fallback.rb
   tool/verify_linux_cjk_font_fallback.rb
@@ -94,6 +96,7 @@ expected_build_needs = %w[
   macos-ip-forwarding
   custom-test-urls
   windows-core-callback
+  proxy-environment
   static-source
   linux-packaging
 ]
